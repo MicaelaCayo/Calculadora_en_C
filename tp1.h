9a00b3d26b47051109059177711ada53a602a1ce
MicaelaCayo <86836904+MicaelaCayo@users.noreply.github.com>
2021-09-25 22:52:51 -0300

    subiendo proyecto

---
+++ tp1.h
@@ -0,0 +1,62 @@
+/*
+ * tp1.h
+ *
+ *  Created on: 25 sep. 2021
+ *      Author: micae
+ */
+
+#ifndef TP1_H_
+#define TP1_H_
+
+
+
+/** \brief suma dos variables usando sus valores otorgados y los almacena el resultado en una direccion de memoria
+ *
+ * \param x int ingrese un numero
+ * \param y int ingrese otro numero
+ * \return int retorna el resultado de la operacion
+ *
+ */
+
+int sumarTP (int x, int y);
+
+/** \brief resta dos variables usando sus valores otorgados y los almacena el resultado en una direccion de memoria
+ *
+ * \param x int ingrese un numero
+ * \param y int ingrese otro numero
+ * \return int retorna el resultado de la operacion
+ *
+ */
+
+int restarTP (int x, int y);
+
+/** \brief divide dos variables usando sus valores otorgados y los almacena el resultado en una direccion de memoria
+ *
+ * \param x int ingrese un numero
+ * \param y int ingrese otro numero
+ * \return float int retorna el resultado de la operacion
+ *
+ */
+
+float dividirTP(int x, int y);
+
+/** \brief multiplica dos variables usando sus valores otorgados y los almacena el resultado en una direccion de memoria
+ *
+ * \param x int ingrese un numero
+ * \param y int ingrese otro numero
+ * \return int retorna el resultado de la operacion
+ */
+
+int multiplicarTP(int x, int y);
+
+/** \brief calcula el factorial y almacena el resultado en una direccion de memoria
+ *
+ * \param int ingrese un numero
+ * \return int retorna el resultado de la operacion
+ *
+ */
+
+int factorialTP (int x);
+
+
+#endif /* TP1_H_ */
